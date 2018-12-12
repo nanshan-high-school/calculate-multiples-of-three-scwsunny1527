ef06c43bddf7a5379cf26a08296c9fa69107aceb
scwsunny1527 <43640660+scwsunny1527@users.noreply.github.com>
2018-12-12 23:00:28 +0800

    Create 3的倍數.cpp

---
+++ 3的倍數.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"輸入數字:";
+    cin>>a;
+
+    if (a%3==0){
+        cout<<"是3的倍數";
+    }
+    else{
+        cout<<"不是3的倍數";
+    }
+}
